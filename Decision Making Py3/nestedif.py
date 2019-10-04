6621e74b3ba3809ad4961a96f6bba82afcb95e76
root <root@localhost.localdomain>
2019-10-04 08:16:27 +0300

    More on python

---
+++ Decision Making Py3/nestedif.py
@@ -0,0 +1,11 @@
+num=int(input("Enter a number"))
+if num2==0:
+    if num3==0:
+        print ("Divisible by 3 and 2")
+    else:
+        print ("Divisible by 2 not divisible by 3")
+else:
+    if num3==0:
+        print ("Divisible by 3 not divisible by 2")
+    else:
+        print ("Not divisible by 2 not divisible by 3")
---
+++ Decision Making Py3/test1.py
@@ -0,0 +1,16 @@
+var1 = 100
+if var1:
+    print ("1 - Got a true expression value")
+    print (var1)
+
+
+var2 = 0
+if (var2==0):
+    print ("2 - Got a true expression value")
+    print (var2)
+
+print ("Good bye!")
+
+#1 - Got a true expression value
+#100
+#Good bye!
