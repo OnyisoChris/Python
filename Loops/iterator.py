ba026e9c53750f32cbf43c40985f3c54a0733b11
OnyisoChris <onyisochris1@gmail.com>
2019-10-18 14:15:28 +0300

    Loops

---
+++ Loops/iterator.py
@@ -0,0 +1,12 @@
+list=[1,2,3,4]
+it = iter(list) # this builds an iterator object
+print (next(it)) # prints next available element in iterator
+# Iterator object can be traversed using regular for statement
+for x in it:
+    print (x, end='')
+#or using the next function
+while True:
+    try:
+        print(next(it))
+    except StopIteration:
+        sys.exit() #you have to import sys module for this.
